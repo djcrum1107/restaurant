b671a454e7c6f9563ec09a7c10eb01280cfa427d
djcrum1107 <djcrum1107@gmail.com>
2021-10-14 21:25:03 -0400

    Updated menu and a single menu item

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,33 @@
+//Create a menu item with name, image, and description
+function createMenuItem(foodName, foodDescription, foodImageLocation){
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menuItem');
+    
+    const foodImage = document.createElement('img');
+    foodImage.src = foodImageLocation;
+
+    const fName = document.createElement('h2');
+    fName.innerText = foodName;
+
+    const fDescription = document.createElement('p');
+    fDescription.innerText = foodDescription;
+    
+    menuItem.appendChild(foodImage);
+    menuItem.appendChild(fName);
+    menuItem.appendChild(fDescription);
+
+    return menuItem;
+}
+
 //The initial creation of the menu content, should only be run once
 function createMenuContent(menuContent){
     menuContent = document.createElement('div');
     menuContent.id = 'menuContent';
+
+    menuContent.appendChild(createMenuItem('Bluey Burger', 
+    'A delicious beef patty with a side of fries for the kids!', 
+    './assets/burger.jpeg'));
+
 
     return menuContent;
 }
