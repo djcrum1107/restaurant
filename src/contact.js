23addf2e4a1d98caea18170966f8f7cdce94997a
djcrum1107 <djcrum1107@gmail.com>
2021-10-14 23:14:13 -0400

    Added contact information page

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,19 @@ function createContactContent(contactContent){
     contactContent = document.createElement('div');
     contactContent.id = 'contactContent';
 
+    const contactBlurb = document.createElement('p1');
+    contactBlurb.innerText = "If you would like to reach out to us don't hesitate to call or email!";
+    contactBlurb.style.fontSize = '30px';
+    contactContent.appendChild(contactBlurb);
+
+    const contactNumber = document.createElement('h2');
+    contactNumber.innerText = "+1 (234) 567-1234"
+    contactContent.appendChild(contactNumber);
+
+    const contactEmail = document.createElement('h2');
+    contactEmail.innerText = "blueyburgers@bb.com"
+    contactContent.appendChild(contactEmail);
+
     return contactContent;
 }
 
